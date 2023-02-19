d7b783dbac6f004324f37da1c35a7c3548b90535
Lordkissa97 <christiaea@uia.no>
2023-02-19 20:06:03 +0100

    innskriving av koder fra kodefragmentene + konverteringsfunksjon på alle 6 temperaturene + main.go kode fra veiledningstimen som er lagt ut på canvas

--- conv/conv.go
+++ conv/conv.go
@@ -9,14 +9,38 @@ package conv
     ...
 */
 
-// Konverterer Farhenheit til Celsius
-func FarhenheitToCelsius(value float64) float64 {
-	// Her skal du implementere funksjonen
-	// Du skal ikke formattere float64 i denne funksjonen
-	// Gjør formattering i main.go med fmt.Printf eller
-	// lignende
-	return 56.67
+// funksjon som konverterer Fahrenheit til Celsius
+func FahrenheitToCelsius(fahrenheit float64) float64 {
+	celsius := (fahrenheit - 32) * 5 / 9
+	return celsius
 }
 
-// De andre konverteringsfunksjonene implementere her
-// ...
+// Funksjonen som konverterer Celsius til Fahrenheit
+func CelsiusToFahrenheit(celsius float64) float64 {
+	fahrenheit := celsius*9/5 + 32
+	return fahrenheit
+}
+
+// Funksjonen som konverterer Kelvin til Celsius
+func KelvinToCelsius(kelvin float64) float64 {
+	celsius := kelvin - 273.15
+	return celsius
+}
+
+// Funksjonen som konverterer Celsius til Kelvin
+func CelsiusToKelvin(celsius float64) float64 {
+	kelvin := celsius + 273.15
+	return kelvin
+}
+
+// Funksjonen som konverterer Kelvin til Fahrenheit
+func KelvinToFahrenheit(kelvin float64) float64 {
+	fahrenheit := (kelvin-273.15)*9/5 + 32
+	return fahrenheit
+}
+
+// Funksjonen som konverterer Fahrenheit til Kelvin
+func FahrenheitToKelvin(fahrenheit float64) float64 {
+	kelvin := (fahrenheit-32)*5/9 + 273.15
+	return kelvin
+}
--- conv/conv_test.go
+++ conv/conv_test.go
@@ -1,37 +1,150 @@
 package conv
 
-import "reflect"
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
+// funksjon som konverterer Fahrenheit til Celsius
+func FahrenheitToCelsius(fahrenheit float64) float64 {
+	celsius := (fahrenheit - 32) * 5 / 9
+	return celsius
+}
 
-/*
-*
-	Mal for testfunksjoner
-	Du skal skrive alle funksjonene basert på denne malen
-	For alle konverteringsfunksjonene (tilsammen 6)
-	kan du bruke malen som den er (du må selvsagt endre
-	funksjonsnavn og testverdier)
-*/
-func TestFarhenheitToCelsius(t *testing.T) {
+func TestFahrenheitToCelsius(t *testing.T) {
 	type test struct {
-		Fahernheit float64
-		Celsius  float64
+		Fahrenheit float64
+		Celsius    float64
 	}
 
-	tests := []test{
-		{Fahernheit: 134, Celsius: 56.67},
+	var tests = []test{
+		{Fahrenheit: 134, Celsius: 56.67},
+		{Fahrenheit: 32, Celsius: 0},
 	}
-
 	for _, v := range tests {
-		got := FarhenheitToCelsius(v.Fahernheit)
+		got := FahrenheitToCelsius(v.Fahrenheit)
 		if !reflect.DeepEqual(got, v.Celsius) {
-			t.Errorf("FarhenheitToCelsius(%v) = %v, want %v", v.Fahernheit, got, v.Celsius)
+			t.Errorf("FahrenheitToCelsius(%v) = %v, want %v", v.Fahrenheit, got, v.Celsius)
 		}
-	}
-
 	}
 }
 
-// De andre testfunksjonene implementeres her
-// ...
+// Funksjonen som konverterer Celsius til Fahrenheit
+func CelsiusToFahrenheit(celsius float64) float64 {
+	fahrenheit := celsius*9/5 + 32
+	return fahrenheit
+}
+
+func TestCelsiusToFahrenheit(t *testing.T) {
+	type test struct {
+		Celsius    float64
+		Fahrenheit float64
+	}
+
+	var tests = []test{
+		{Celsius: 56.67, Fahrenheit: 134},
+		{Celsius: 0, Fahrenheit: 32},
+	}
+	for _, v := range tests {
+		got := CelsiusToFahrenheit(v.Celsius)
+		if !reflect.DeepEqual(got, v.Fahrenheit) {
+			t.Errorf("CelsiusToFahrenheit(%v) = %v, want %v", v.Celsius, got, v.Fahrenheit)
+		}
+	}
+}
+
+// Funksjonen som konverterer Kelvin til Celsius
+func KelvinToCelsius(kelvin float64) float64 {
+	celsius := kelvin - 273.15
+	return celsius
+}
+
+func TestKelvinToCelsius(t *testing.T) {
+	type test struct {
+		Kelvin  float64
+		Celsius float64
+	}
+
+	var tests = []test{
+		{Kelvin: 329.82, Celsius: 56.67},
+		{Kelvin: 273.15, Celsius: 0},
+	}
+	for _, v := range tests {
+		got := KelvinToCelsius(v.Kelvin)
+		if !reflect.DeepEqual(got, v.Celsius) {
+			t.Errorf("KelvinToCelsius(%v) = %v, want %v", v.Kelvin, got, v.Celsius)
+		}
+	}
+}
+
+// Funksjonen som konverterer Celsius til Kelvin
+func CelsiusToKelvin(celsius float64) float64 {
+	kelvin := celsius + 273.15
+	return kelvin
+}
+
+func TestCelsiusToKelvin(t *testing.T) {
+	type test struct {
+		Celsius float64
+		Kelvin  float64
+	}
+
+	var tests = []test{
+		{Celsius: 56.67, Kelvin: 329.82},
+		{Celsius: 0, Kelvin: 273.15},
+	}
+	for _, v := range tests {
+		got := CelsiusToKelvin(v.Celsius)
+		if !reflect.DeepEqual(got, v.Kelvin) {
+			t.Errorf("CelsiusToKelvin(%v) = %v, want %v", v.Celsius, got, v.Kelvin)
+		}
+	}
+}
+
+// Funksjonen som konverterer Kelvin til Fahrenheit
+func KelvinToFahrenheit(kelvin float64) float64 {
+	fahrenheit := (kelvin-273.15)*9/5 + 32
+	return fahrenheit
+}
+
+func TestKelvinToFahrenheit(t *testing.T) {
+	type test struct {
+		Kelvin     float64
+		Fahrenheit float64
+	}
+
+	var tests = []test{
+		{Kelvin: 329.82, Fahrenheit: 134},
+		{Kelvin: 273.15, Fahrenheit: 32},
+	}
+	for _, v := range tests {
+		got := KelvinToFahrenheit(v.Kelvin)
+		if !reflect.DeepEqual(got, v.Fahrenheit) {
+			t.Errorf("KelvinToFahrenheit(%v) = %v, want %v", v.Kelvin, got, v.Fahrenheit)
+		}
+	}
+}
+
+// Funksjonen som konverterer Fahrenheit til Kelvin
+func FahrenheitToKelvin(fahrenheit float64) float64 {
+	kelvin := (fahrenheit-32)*5/9 + 273.15
+	return kelvin
+}
+
+func TestFahrenheitToKelvin(t *testing.T) {
+	type test struct {
+		Fahrenheit float64
+		Kelvin     float64
+	}
+
+	var tests = []test{
+		{Fahrenheit: 134, Kelvin: 329.82},
+		{Fahrenheit: 32, Kelvin: 273.15},
+	}
+	for _, v := range tests {
+		got := FahrenheitToKelvin(v.Fahrenheit)
+		if !reflect.DeepEqual(got, v.Kelvin) {
+			t.Errorf("FahrenheitToKelvin(%v) = %v, want %v", v.Fahrenheit, got, v.Kelvin)
+		}
+	}
 }
